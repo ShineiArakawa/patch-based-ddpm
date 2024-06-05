a0006f3f5ae64193e9b1bc0f41f6e73a191d0894
Shinei Arakawa <araka@s184.lab>
2024-06-06 01:51:36 +0900

    Update source codes.

--- scripts/run_sample.sh
+++ scripts/run_sample.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-DIVECE_ID=0,1,2,3
+DIVECE_ID=4,5,6,7
 CONFIG_FILE="configs/config_patch_divider=4.json"
-CHECKPOINT="log/celeba128/checkpoints/model-4000.pt"
-OUT_DIR="sampled/celeba128_4x4"
-NUM_SAMPLES=32
+CHECKPOINT="logs/celeba128/checkpoints/model-last.pt"
+OUT_DIR="sampled/celeba128-4x4"
+NUM_SAMPLES=16
 BATCH_SIZE=4
 
 CUDA_VISIBLE_DEVICES=${DIVECE_ID} accelerate launch -m patch_based_ddpm.sample \
--- scripts/run_train.sh
+++ scripts/run_train.sh
@@ -2,6 +2,8 @@
 
 DIVECE_ID=4,5,6,7
 CONFIG_FILE="configs/config_patch_divider=4.json"
+RESTART_STEP=1 # '1' means starting from scratch
 
 CUDA_VISIBLE_DEVICES=${DIVECE_ID} accelerate launch -m patch_based_ddpm.train \
-    --config ${CONFIG_FILE}
+    --config ${CONFIG_FILE} \
+    --restart-step ${RESTART_STEP}
